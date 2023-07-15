e49ed3cc56aedee0303c667d861f70fa77ef4977
andrewbidylo <bidyloandrew@gmail.com>
2023-07-14 18:45:06 -0700

    add TypeORM to connect PG and user entity

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,30 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { FilesModule } from './files/files.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './users/entities/user.entity';
 
 @Module({
-  imports: [UsersModule, FilesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_LOGIN,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [UserEntity],
+      synchronize: true,
+    }),
+    UsersModule,
+    FilesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
