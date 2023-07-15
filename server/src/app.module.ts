0205746fdbcd81d92e95626c062987c683369e4d
andrewbidylo <bidyloandrew@gmail.com>
2023-07-14 20:30:41 -0700

    update app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -10,13 +10,11 @@ import { FileEntity } from './files/entities/file.entity';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT) || 5432,
+      port: Number(process.env.DB_PORT) || 5432,
       username: process.env.DB_LOGIN,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
