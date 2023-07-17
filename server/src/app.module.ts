05f11e01bb63d719a102ab56a313ecb7bca5b5c2
andrewbidylo <bidyloandrew@gmail.com>
2023-07-17 14:00:41 -0700

    ganarate auth modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,6 +7,7 @@ import { FilesModule } from './files/files.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './users/entities/user.entity';
 import { FileEntity } from './files/entities/file.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { FileEntity } from './files/entities/file.entity';
     }),
     UsersModule,
     FilesModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({
+    default: 'test@test.com',
+  })
+  email: string;
+
+  @ApiProperty({
+    default: 'Mr. Boo',
+  })
+  fullName: string;
+
+  @ApiProperty({
+    default: '123',
+  })
+  password: string;
+}
