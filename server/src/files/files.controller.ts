e46a1f7518c5c8164433cd0b32a1bb105064c035
andrewbidylo <bidyloandrew@gmail.com>
2023-07-17 13:46:43 -0700

    get all files

--- server/src/files/files.controller.ts
+++ server/src/files/files.controller.ts
@@ -5,17 +5,24 @@ import {
   UploadedFile,
   ParseFilePipe,
   MaxFileSizeValidator,
+  Get,
+  Query,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileStorage } from './storage';
 import { UserId } from '../decorators/user-id.decorator';
+import { FileType } from './entities/file.entity';
 
 @Controller('files')
 @ApiTags('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+  @Get()
+  findAll() {
+    return this.filesService.findAll();
+  }
 
   @Post()
   @UseInterceptors(
@@ -44,6 +51,6 @@ export class FilesController {
     file: Express.Multer.File,
     @UserId() userId: number,
   ) {
-    return this.filesService.create(file);
+    return this.filesService.create(file, userId);
   }
 }
